8d58521638a72c469f6df5f9630849d65b77a00b
EsmeRivas <esme99rivas@gmail.com>
2024-07-22 00:06:29 -0600

    hotcommit

--- resources/views/tocas/index.blade.php
+++ resources/views/tocas/index.blade.php
@@ -24,6 +24,8 @@
                 <table id="my_table" class="table table-hover text-nowrap">
                     <thead>
                     <tr>
+                        <th></th>
+
                         <th>No.Toca</th>
 
                         <th>No.Proceso<br>(expediente)</th>
@@ -57,6 +59,14 @@
                     @csrf
                     @foreach($tocas as $toca)
                         <tr>
+                            <td>
+                                <button data-id='' type='button' class='btn btn-success toca-finalizado btn-sm' data-bs-toggle="tooltip" title="Finalizar toca">
+                                    <i data-id='' class="fas fa-check toca-finalizado"></i>
+                                </button>
+                                <button data-id='' type='button' class='btn btn-warning registrar-amparo btn-sm' data-bs-toggle="tooltip" title="Registrar amparo" data-toggle="modal" data-target="#miModal">
+                                    <i data-id='' class="fas fa-file registrar-amparo"></i>
+                                </button>
+                            </td>
                             <td>{{$toca->numero_toca}}</td>
                             <td>{{$toca->numero_expediente}}</td>
                             <td class="ajuste-texto">{{$toca->imputado}}</td>
@@ -80,11 +90,72 @@
                 </table>
             </div>
         </div>
+        <!-- Modal -->
+        <div class="modal" id="miModal">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <!-- Encabezado del modal -->
+                    <div class="modal-header">
+                      <h5 class="modal-title">Nuevo amparo - Toca No. 1</h5>
+                      <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <!-- Contenido del modal -->
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="num_amparo">No de Amparo</label>
+                            <input type="text" class="form-control" id="numero_amparo" placeholder="Escribe el numero de amparo" name="numero_amparo">
+                        </div>
+                        <div class="form-group">
+                            <label for="num_oficio">No de Oficio</label>
+                            <input type="text" class="form-control" id="numero_oficio" placeholder="Escribe el numero de oficio" name="numero_oficio">
+                        </div>
+                        <div class="form-group">
+                            <label for="colegiado">Colegiado</label>
+                            <input type="text" class="form-control" id="input-colegiado" placeholder="Escribe el colegiado" name="colegiado">
+                        </div>
+                        <div class="form-group">
+                            <label for="quejoso">Quejoso</label>
+                            <select id="mesa_id" name="mesa_id" class="form-control">
+                                <option value="" disabled selected>Seleccione una opción</option>
+                                <option value="1">Jose Alexis Vazquez Morales</option>
+                                <option value="2">Gloria Romo</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="termino">Fecha de termino</label>
+                            <span class="badge badge-success badge-pill">Vence al rededor de 15 días</span>
+                            <span class="badge badge-warning badge-pill">Vence al rededor de 10 días</span>
+                            <span class="badge badge-danger badge-pill">Vence al rededor de 3 días</span>
+                            <input type="date" class="form-control" id="input-termino" name="termino">
+                        </div>
+                    </div>
+                    <!-- Pie del modal -->
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                      <button type="button" class="btn btn-primary">Registrar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection('content')
 @section('scripts')
 <script>
+    // Obtener la fecha actual y agregar 15 días
+    var fechaActual = new Date();
+    fechaActual.setDate(fechaActual.getDate() + 15);
+    
+    // Formatear la fecha en el formato requerido por el input de tipo date
+    var yyyy = fechaActual.getFullYear();
+    var mm = String(fechaActual.getMonth() + 1).padStart(2, '0');
+    var dd = String(fechaActual.getDate()).padStart(2, '0');
+    var fechaLimite = yyyy + '-' + mm + '-' + dd;
+    
+    // Establecer el límite mínimo y máximo del input de tipo date
+    document.getElementById('input-termino').min = fechaLimite;
+    document.getElementById('input-termino').max = fechaLimite;
+
     //convertir a mayúsculas el texto ingresado en formulario
     function convertirMayusculas(input) {
         input.value = input.value.toUpperCase();
